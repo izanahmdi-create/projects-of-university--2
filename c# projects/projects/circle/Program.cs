49dcc66ebea3bfb56ba7fb72ec457f83a6143550
izanahmdi-create <izanahmdi@gmail.com>
2025-10-22 08:10:12 +0330

    Add files via upload

---
+++ c# projects/projects/circle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter radius: ");
+        double r = double.Parse(Console.ReadLine());
+
+        double p = 2 * Math.PI * r;
+        double a = Math.PI * r * r;
+
+        Console.WriteLine("Perimeter = " + p);
+        Console.WriteLine("Area = " + a);
+
+        Console.ReadKey();
+    }
+}
---
+++ c# projects/projects/temperature/Program.cs
@@ -0,0 +1,12 @@
+﻿
+    {
+        Console.Write("Enter temperature in Celsius: ");
+        double c = double.Parse(Console.ReadLine());
+
+        double f = (c * 9 / 5) + 32;
+
+        Console.WriteLine("Temperature in Fahrenheit = " + f);
+
+        Console.ReadKey();
+    }
+
