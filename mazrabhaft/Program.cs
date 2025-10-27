7e3e72c88ded8c364d6236b43f44ade4d1ecd5f9
izanahmdi-create <izanahmdi@gmail.com>
2025-10-27 20:18:02 +0330

    Add files via upload

---
+++ mazrabhaft/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MultipleOf7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 7 == 0)
+            {
+                Console.WriteLine("!Yes");
+            }
+            else
+            {
+                int result = num * 3;
+                Console.WriteLine("Result = " + result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ nouneven/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CheckNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 10 && num % 2 == 0)
+            {
+                Console.WriteLine("!True");
+            }
+            else
+            {
+                Console.WriteLine("not valid.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
