a0bacece058ebe3c1b6206cb427d0c494c57039d
izanahmdi-create <izanahmdi@gmail.com>
2025-11-22 03:29:47 +0330

    Add files via upload

---
+++ 4th practice-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = new int[10];
+        int even = 0;
+        int odd = 0;
+
+        Console.WriteLine("Enter 10 numbers:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Number " + (i + 1) + ": ");
+            nums[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (nums[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        Console.WriteLine("Even numbers: " + even);
+        Console.WriteLine("Odd numbers: " + odd);
+    }
+}
---
+++ 4th practice-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = new int[5];
+        bool found = false;
+        int position = -1;
+
+        Console.WriteLine("Enter 5 numbers:");
+
+        // Read 5 numbers
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Number " + (i + 1) + ": ");
+            nums[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Search for number 2
+        for (int i = 0; i < 5; i++)
+        {
+            if (nums[i] == 2)
+            {
+                found = true;
+                position = i + 1;  // +1 to convert from index to human-readable
+                break;
+            }
+        }
+
+        // Output result
+        if (found)
+            Console.WriteLine("Number 2 exists and it is number " + position + " in the list.");
+        else
+            Console.WriteLine("Number 2 does NOT exist in the list.");
+    }
+}
