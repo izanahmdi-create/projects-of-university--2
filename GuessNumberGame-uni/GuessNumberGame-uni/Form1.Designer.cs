72d68bcb4308ffe2cdbe3c65bff61fae9dea5fe2
izanahmdi-create <izanahmdi@gmail.com>
2025-12-29 12:22:40 +0330

    Add files via upload

---
+++ GuessNumberGame-uni/GuessNumberGame-uni/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace GuessNumberGame_uni
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblGuess = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblTry = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtPlayer = new System.Windows.Forms.TextBox();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblGuess
+            // 
+            this.lblGuess.AutoSize = true;
+            this.lblGuess.Location = new System.Drawing.Point(393, 190);
+            this.lblGuess.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGuess.Name = "lblGuess";
+            this.lblGuess.Size = new System.Drawing.Size(108, 20);
+            this.lblGuess.TabIndex = 3;
+            this.lblGuess.Text = "Guess Number:";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtGuess.Enabled = false;
+            this.txtGuess.Location = new System.Drawing.Point(362, 215);
+            this.txtGuess.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(155, 27);
+            this.txtGuess.TabIndex = 4;
+            this.txtGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.BackColor = System.Drawing.Color.SpringGreen;
+            this.btnCheck.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCheck.Enabled = false;
+            this.btnCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCheck.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnCheck.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCheck.Location = new System.Drawing.Point(406, 262);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(74, 28);
+            this.btnCheck.TabIndex = 5;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(32, 330);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(52, 20);
+            this.lblResult.TabIndex = 6;
+            this.lblResult.Text = "Result:";
+            // 
+            // lblTry
+            // 
+            this.lblTry.AutoSize = true;
+            this.lblTry.Location = new System.Drawing.Point(777, 330);
+            this.lblTry.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTry.Name = "lblTry";
+            this.lblTry.Size = new System.Drawing.Size(57, 20);
+            this.lblTry.TabIndex = 7;
+            this.lblTry.Text = "Tries=0";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(420, 330);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(60, 20);
+            this.lblTime.TabIndex = 8;
+            this.lblTime.Text = "Time=0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStart.Location = new System.Drawing.Point(91, 65);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(74, 27);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtPlayer
+            // 
+            this.txtPlayer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPlayer.Location = new System.Drawing.Point(47, 28);
+            this.txtPlayer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPlayer.Name = "txtPlayer";
+            this.txtPlayer.Size = new System.Drawing.Size(155, 27);
+            this.txtPlayer.TabIndex = 1;
+            this.txtPlayer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPlayer.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.Location = new System.Drawing.Point(78, 3);
+            this.lblPlayer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(96, 20);
+            this.lblPlayer.TabIndex = 0;
+            this.lblPlayer.Text = "Player Name:";
+            this.lblPlayer.Click += new System.EventHandler(this.lblPlayer_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.txtPlayer);
+            this.panel1.Controls.Add(this.lblPlayer);
+            this.panel1.Location = new System.Drawing.Point(314, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(254, 110);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1096, 692);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblTry);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblGuess);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guess Number Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblGuess;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblTry;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtPlayer;
+        private System.Windows.Forms.Label lblPlayer;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ GuessNumberGame-uni/GuessNumberGame-uni/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNumberGame_uni
+{
+    public partial class Form1 : Form
+    {
+        int randomNumber;
+        int TryCount = 0;
+        int Time = 0;
+        string playerName;
+        public Form1()
+        {
+            InitializeComponent();
+            txtGuess.Enabled = false;
+            btnCheck.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!txtPlayer.Text.All(char.IsLetter))
+            {
+                MessageBox.Show("Name must include characters!");
+                return;
+            }
+           
+
+                playerName = txtPlayer.Text;
+
+            Random rnd = new Random();
+            randomNumber = rnd.Next(1, 101);
+
+            TryCount = 0;
+            Time = 0;
+
+            lblTry.Text = "Tries:0";
+            lblTime.Text = "Time: 0";
+
+            txtGuess.Enabled = true;
+            btnCheck.Enabled = true;
+
+            timer1.Start();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            int Guess;
+            if (!int.TryParse(txtGuess.Text, out Guess))
+            {
+                MessageBox.Show("Enter the number please");
+                return;
+            }
+
+            TryCount++;
+            lblTry.Text = "Tries: " + TryCount;
+            if (Guess > randomNumber)
+            {
+                lblResult.Text = "the number is greater than the default!";
+                lblResult.ForeColor = Color.Red;
+            }
+            else if (Guess < randomNumber)
+            {
+                lblResult.Text = " the number is lesser than the default!";
+                lblResult.ForeColor = Color.Red;
+            }
+            else
+            {
+                lblResult.ForeColor = Color.Green;
+                timer1.Stop();
+                int score = CalculateScore();
+                SaveToFile(playerName, score, Time);
+                MessageBox.Show($"Congratulations{playerName}!\n Score:{score}\n Time:{Time}");
+
+                btnCheck.Enabled = false;
+                txtGuess.Enabled = false;
+
+            }
+            txtGuess.Clear();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Time++;
+            lblTime.Text = "The time is: " + Time;
+        }
+        int CalculateScore()
+        {
+            if (TryCount < 5)
+                return 40;
+            else if (TryCount <= 10)
+                return 20;
+            else
+                return 10;
+
+        }
+        void SaveToFile(string player, int score, int time)
+        {
+            try
+            {
+                string folderPath = @"C:\202";
+                string filePath = @"C:\202\Num.txt";
+
+                // ساخت پوشه (اگر وجود نداشته باشد)
+                Directory.CreateDirectory(folderPath);
+
+                string text =
+                    $"Player: {player} | Score: {score} | Time: {time} Sec | {DateTime.Now}\n";
+
+                File.AppendAllText(filePath, text);
+
+                MessageBox.Show("Information saved ✅");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:\n" + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPlayer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
